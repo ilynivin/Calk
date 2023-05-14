e4e6058fbba4ecfda8d26dcd48b565be5b090cc1
Nivins <nivinsvysakh2020@outlook.com>
2023-05-14 21:33:12 +0530

    added files

---
+++ main.py
@@ -0,0 +1,33 @@
+import math
+
+print("\t Hello Welcome to Calk!")
+print("\t Read my doc page on Github  \n \tDont Fork me ! Give it a Star!")
+
+a = int(input("Enter Number A :"))
+b = int(input("Enter Number B :"))
+Op = input("Enter Operation :")
+
+if Op == '+' :
+    print("The Answer is :", a+b)
+
+if Op == '-':
+    print("The Answer is :", a-b)
+
+if Op =='*':
+    print("The Answer is :", a*b)
+
+if Op == '/':
+    print("The Answer is :", a/b)
+
+if Op == '%':
+    print("The Answer is :", a%b)
+
+if Op == '**':
+    print("The Answer is :" , a**b)
+
+if Op == '//':
+    print("The Answer is :" , a//b)
+
+
+
+
