ddf5bd3ae964b2e93d16d59b8061cbca0cf3c3fc
Nivin s <nivinsvysakh2020@outlook.com>
2023-05-15 08:21:05 +0530

    Added Error conditions on the calk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 
 print("\t Hello Welcome to Calk!")
-print("\t Read my doc page on Github https://github.com/Nivin389/Calk#readme \n \tDont Fork me ! Give it a Star!")
+print("\t Read my doc page on Github  \n https://github.com/Nivin389/Calk#readme \n\tDont Fork me ! Give it a Star!")
 
 a = int(input("Enter Number A :"))
 b = int(input("Enter Number B :"))
@@ -9,22 +9,36 @@ Op = input("Enter Operation :")
 
 if Op == '+' :
     print("The Answer is :", a+b)
+    print("Thank you For Using me ! Please Give it a Star ")
 
 if Op == '-':
     print("The Answer is :", a-b)
+    print("Thank you For Using me ! Please Give it a Star ")
 
 if Op =='*':
     print("The Answer is :", a*b)
+    print("Thank you For Using me ! Please Give it a Star ")
 
 if Op == '/':
     print("The Answer is :", a/b)
+    print("Thank you For Using me ! Please Give it a Star ")
 
 if Op == '%':
     print("The Answer is :", a%b)
+    print("Thank you For Using me ! Please Give it a Star ")
 
 if Op == '**':
     print("The Answer is :" , a**b)
+    print("Thank you For Using me ! Please Give it a Star ")
 
 if Op == '//':
     print("The Answer is :" , a//b)
+    print("Thank you For Using me ! Please Give it a Star ")
 
+# Error catch
+
+if a == 0 and b == 0:
+    print("Error! 2 inputs can't be 0")
+
+if Op.isnumeric():
+    print("Error! Invalid Operation\nPlease Look my Doc page on Github\n https://github.com/Nivin389/Calk#readme")
